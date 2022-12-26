b7cb81b9d4991845f036fe6eae52b50e2adf7495
nour al maleh <nourmale7@gmail.com>
2022-12-26 23:25:23 +0300

    nothing important

---
+++ src/components/blogs/subscribe.js
@@ -0,0 +1,37 @@
+
+import { useState } from "react";
+import { Auth } from "../../Firebase";
+
+
+
+
+function Subscribeform(){
+    const [email , setEmail] = useState("")
+
+    const  Handlesubmit = (e)=>{
+e.preventDefault();
+Auth.collection("subscriptions").add({
+    email: email
+})}
+
+
+
+
+
+
+
+
+return (
+
+<form>
+    <label htmlFor='email'>
+        <input type="email" placeholder="Enter your e-mail" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+    </label>
+    <button type="submit" onSubmit={Handlesubmit}>submit</button>
+</form>
+
+)
+}
+
+
+export default Subscribeform
