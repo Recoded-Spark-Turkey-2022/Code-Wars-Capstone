231d744be4224ea08d0987cc126fdd46f736fea8
NourMaleh <113291094+NourMaleh@users.noreply.github.com>
2022-12-29 20:45:43 +0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,15 +1,20 @@
-import { initializeApp } from "firebase/app";  
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-lIwSHr8JegZMANEQv05TnJklZfSNBHc",
-    authDomain: "code-wars-capstone.firebaseapp.com",
-    projectId: "code-wars-capstone",
-    storageBucket: "code-wars-capstone.appspot.com",
-    messagingSenderId: "435500261308",
-    appId: "1:435500261308:web:99669583edce4ecd89d5b9"
-  };
+  apiKey: "AIzaSyB-lIwSHr8JegZMANEQv05TnJklZfSNBHc",
+  authDomain: "code-wars-capstone.firebaseapp.com",
+  projectId: "code-wars-capstone",
+  storageBucket: "code-wars-capstone.appspot.com",
+  messagingSenderId: "435500261308",
+  appId: "1:435500261308:web:99669583edce4ecd89d5b9"
+};
 
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export default app
+
+export default db;
