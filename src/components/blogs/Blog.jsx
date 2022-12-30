66e9fc377281ff29ab625222e91104d647cedc74
nour al maleh <nourmale7@gmail.com>
2022-12-31 00:15:17 +0300

    blogs articles added

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -1,10 +1,14 @@
 import Subscribeform from "./Subscribe";
+import BlogData from './Blogdata';
+import Blogarticle from "./Blogarticle";
 
 function Blog (){
-
+    const Article = BlogData[0];
     return(
         <>
-    <h1>hello world</h1>
+    <Blogarticle  imageurl={Article.imageurl} header={Article.header} paragraph={Article.paragraph} header1={Article.header1} paragraph2={Article.paragraph2}
+    paragraph3={Article.paragraph3} paragraph4={Article.paragraph4}
+     />
         <Subscribeform />
         </> 
     )
---
+++ src/components/blogs/Blogarticle.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+ function Blogarticle ({id,imageUrl,header,header1,paragraph,paragraph2,paragraph3,paragraph4}) {
+
+  return (
+    <div id={id}>
+<img src={imageUrl} alt="article"/>
+<div>
+<h2>{header}</h2>
+<p>{paragraph}</p>
+</div>
+<div>
+<h4>{header1}</h4>
+<p>{paragraph2}</p>
+</div>
+<div>
+<p>{paragraph3}</p></div>
+<div>
+<p>{paragraph4}</p></div>
+
+<div>
+  <h3>Sign up for The Healing blog</h3>
+  <p>A weekly, ad-free Blog that helps you stay in the know.</p>
+</div>
+<h3>Recommended for you</h3>
+    </div>
+  )
+}
+
+export default Blogarticle;
