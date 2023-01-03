3cd4e20272080b9b03909ae127bcc4f61272b345
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2023-01-03 11:52:18 +0300

    Test

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './store/store';
 import App from './App';
 import './index.css';
 
@@ -11,10 +8,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
