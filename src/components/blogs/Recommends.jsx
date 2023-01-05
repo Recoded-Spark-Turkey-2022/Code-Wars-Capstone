aa9a1ffece9496a5403a2a50ba13fbe721685187
Muhammed Mustafa <h.mmustafa98@gmail.com>
2023-01-05 19:23:22 +0300

    route issue fixed

--- src/components/blogs/Recommends.jsx
+++ src/components/blogs/Recommends.jsx
@@ -1,17 +1,27 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import {  useNavigate } from "react-router-dom"
 import second from "./2nd.jpg"
 import third from "./3rd.jpg"
 
 
 function Recommends (){
+    const navigate = useNavigate();
+    const handleBlog2 =(e)=>{
+        e.preventDefault();
+        navigate(`/blog2`)
 
+    }
+
+    const handleBlog3 =(e)=>{
+        e.preventDefault();
+        navigate(`/blog3`)
+    }
     return(
     <div className="ml-12 w-[75rem] h-[35rem] pt-[10rem] "   >
             <p className=" text-3xl uppercase ">Recommended for you</p>
             <div className="flex space-x-1">
-        <Link className="pt-10 rounded  w-[70rem] h-[35rem]"  to="blog2"> <img src={second} alt="second"  />  </Link>
-        <Link className="pt-10 rounded w-[70rem] h-[35rem]"  to="blog3"> <img src={third} alt="third"/>  </Link>
+        <button  type="submit" onClick={handleBlog2} className="pt-10 rounded  w-[70rem] h-[35rem]"  to="blog2"> <img src={second} alt="second"  />  </button>
+        <button type="submit" onClick={handleBlog3} className="pt-10 rounded w-[70rem] h-[35rem]"  to="blog3"> <img src={third} alt="third"/>  </button>
         </div>
         </div>
     )
