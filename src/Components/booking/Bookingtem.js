ae653ab89ec36d1f9a959cf75e0fe36def52b773
nour al maleh <nourmale7@gmail.com>
2023-01-17 22:30:08 +0300

    some styling not finished yet i will work on fanctunality

--- src/Components/booking/Bookingtem.js
+++ src/Components/booking/Bookingtem.js
@@ -16,16 +16,18 @@ const Bookingtem = ({ question, choices }) => {
   // }
 
   return (
-    <div className='justify-items-center'>
-      <h1 className="text-5xl uppercase" > let&apos;s match you with the right therapist </h1>
+    <div >
+      <div className='pl-[3rem] pt-[3rem] pb-[3rem]'><h1 className="text-5xl uppercase" > let&apos;s match you with the right therapist </h1>
   <p className=' text-2xl text-justify text-black text-opacity-50 lg:w-[65rem]'>Please fill out this short questionnaire to provide some general and anonymous background about you and the issues you&apos;d like to deal with in online therapy. It would help us match you 
-  <br/>with the most suitable therapist for you.</p>
-  <div className='inline-flex flex-col space-y-8 items-center justify-start px-10 pt-8 pb-64 bg-white shadow rounded'>
+  <br/>with the most suitable therapist for you.</p></div>
+  <div className=' grid justify-items-center'>
+  <div className=' flex-col space-y-8 grid justify-items-start px-10 pt-8 pb-[10rem] bg-white shadow rounded'>
       <h1 className='text-3xl capitalize'>{question}</h1>
       <div>{choices.map((choice) => (
-        <button type='submit' className='  flex items-center justify-start py-4 pl-6 pr-56 bg-white border rounded-md border-gray-300  hover:bg-cyan-500 translate-y-1' >{choice}</button>
+        <button type='submit' className='pl-[1rem] text-xl text-center capitalize flex items-center lg:w-[30rem] space-x-2 h-[4rem] bg-white border rounded-md border-gray-300  hover:bg-cyan-500 translate-y-1' >{choice}</button>
       ))}</div>
-      <button className='md:text-lg ml-2  lg:px-6 rounded-md box-border  transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 translate-y-1 hover:text-white ' type='submit'>next</button>
+      <button className='inline-block align-bottom uppercase md:text-lg ml-2  lg:px-6 rounded-md box-border  transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 translate-y-1 hover:text-white ' type='submit'>next</button>
+      </div>
       </div>
       </div>
   );
