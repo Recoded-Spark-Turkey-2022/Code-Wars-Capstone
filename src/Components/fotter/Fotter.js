6f12ec9cb83b9a3e039c1938ae0c6f9754774100
nour al maleh <nourmale7@gmail.com>
2023-01-22 23:16:21 +0300

    bg amber

--- src/Components/fotter/Fotter.js
+++ src/Components/fotter/Fotter.js
@@ -8,7 +8,7 @@ function Footer (){
 
 return (
 
- <div className="footer-basic bg-yellow-200">
+ <div className="footer-basic bg-amber-200">
        <footer className="p-4 bg-white rounded-lg  md:flex md:items-center justify-between md:p-6 bg-transparent mx-20 ">
         <div className="m-4">
     <h1 className="font-bold lg:text-4xl md:text-2xl sm:text-sm">Subscribe
