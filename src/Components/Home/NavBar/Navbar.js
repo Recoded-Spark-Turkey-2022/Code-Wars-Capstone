4f940c5dcc2a815d08f2d2c910c0563ca699fe65
nour al maleh <nourmale7@gmail.com>
2023-01-30 20:48:34 +0300

    no changes

--- src/Components/Home/NavBar/Navbar.js
+++ src/Components/Home/NavBar/Navbar.js
@@ -38,8 +38,13 @@ const onclicklogo = ()=>{
 }
 
 return (
+  <div>
+  <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <span className="sr-only">Open main menu</span>
+      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd">n</path></svg>
+    </button>
+
      <nav style={{fontFamily: 'Poppins, sans-serif'}} className="bg-cyan-50 px-2 sm:px-4 py-2.5 rounded   ">
-        
         <button type="submit" onClick={onclicklogo}> <img src={Image} alt="logo" className=" h-9 w-9 mt-2 ml-6"  /></button>
         <Link to="/"> <h1 className="absolute top-4 left-[5.5rem]  text-3xl" >Healing</h1> </Link>
        
@@ -80,7 +85,7 @@ return (
              </div>
         
      </nav>
-
+     </div>
 );
 }
 export default Navebar;
