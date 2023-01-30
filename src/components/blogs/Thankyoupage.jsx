591b5a46b6b86b35b6045759bb903e4116eaea0c
nour al maleh <nourmale7@gmail.com>
2023-01-31 01:05:04 +0300

    mb

--- src/components/blogs/Thankyoupage.jsx
+++ src/components/blogs/Thankyoupage.jsx
@@ -5,7 +5,7 @@ function Thankyoupage({ text, header }) {
   const navigate = useNavigate();
   
   return (
-    <div className="pl-[5rem] mt-[3rem] ">
+    <div className="pl-[5rem] mt-[3rem] mb-5 ">
       <h1 className="pl-15 xl:w-72 xl:text-5xl  md:w-50 md:text-4xl sm:w-30 sm:text-2xl uppercase">
         {header}
       </h1>
@@ -13,10 +13,10 @@ function Thankyoupage({ text, header }) {
         {text}
       </p>
 
-      <div className="lg:w-72 lg:h-14 md:w-45 md:h-10 sm:w-40 sm:h-7 mr-20 ">
+      <div className="lg:w-72 lg:h-14 md:w-45 md:h-10 sm:w-40 sm:h-7 mr-20  ">
       <div className="flex items-center justify-center flex-1 py-3  transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 hover:text-white rounded-md">
           <button
-            className=" h-full xl:text-2xl sm:text-l text-center uppercase"
+            className=" h-full xl:text-2xl sm:text-l text-center uppercase "
             type="submit"
             onClick={() => navigate('/')}
           >
