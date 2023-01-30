a45e6737488fe4a651e81cab919635733c9504e4
Muhanned Noman <Muhanned.Noman@gmail.com>
2023-01-30 21:34:49 +0300

    Fix: update app.jsx to latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navebar from './Components/Home/NavBar/Navbar';
-import Login from "./Components/login/Login";
-import Home from "./Components/Home/Home";
-import Signup from './Components/Signup';
-// import Thankyoupage from './Components/blogs/Thankyoupage';
-import Blogarticle from './Components/blogs/Blogarticle';
-import UpdateUserInfo from './Components/UpdateUserInfo/UpdateUserInfo'
-import Profilepic from "./Components/UpdateUserInfo/Profilepic"
-import Bookingtem from './Components/booking/Bookingtem';
-import Bookingradio from './Components/booking/Bookingradio';
-import Note from "./Components/booking/Note"
-import Submit from './Components/booking/Submit';
-import Requestsubmit from './Components/booking/RequestSubmit';
-import Fotter from "./Components/fotter/Fotter"
-import Team from './Components/about/Team';
-import About from './Components/about/About';
-import ContactUs from './Components/contactus/Contact';
-import Careers from './Components/about/Careers';
+import Navebar from './components/Home/NavBar/Navbar';
+import Login from "./components/login/Login";
+import Home from "./components/Home/Home";
+import Signup from './components/Signup';
+// import Thankyoupage from './components/blogs/Thankyoupage';
+import Blogarticle from './components/blogs/Blogarticle';
+import UpdateUserInfo from './components/UpdateUserInfo/UpdateUserInfo'
+import Profilepic from "./components/UpdateUserInfo/Profilepic"
+import Bookingtem from './components/booking/Bookingtem';
+import Bookingradio from './components/booking/Bookingradio';
+import Note from "./components/booking/Note"
+import Submit from './components/booking/Submit';
+import Requestsubmit from './components/booking/RequestSubmit';
+import Fotter from "./components/fotter/Fotter"
+import Team from './components/about/Team';
+import About from './components/about/About';
+import ContactUs from './components/contactus/Contact';
+import Careers from './components/about/Careers';
 
 
 function App() {
