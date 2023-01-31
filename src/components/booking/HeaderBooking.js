3aa6d6ad7ccbde8db2c5359a5b415207efdec447
nour al maleh <nourmale7@gmail.com>
2023-01-31 21:58:18 +0300

    styling bug

--- src/components/booking/HeaderBooking.js
+++ src/components/booking/HeaderBooking.js
@@ -1,7 +1,7 @@
 const HeaderBooking=({Header , par})=> {
     return (
 
-        <div style={{fontFamily: 'Poppins, sans-serif'}}><h1 className="pb-[0.5rem] text-[2.6rem] tracking-wider md:text-2x sm:text-lg mt-10 mb-2 ml-20 md:ml-10 uppercase " > {Header} </h1>
+        <div style={{fontFamily: 'Poppins, sans-serif'}}><h1 className="pb-[0.5rem] lg:text-[2.6rem] tracking-wider md:text-2x sm:text-lg mt-10 mb-2 ml-20 md:ml-10 uppercase " > {Header} </h1>
         <p className='tex-xl md:text-xl text-stone-500 sm:text-base ml-20 mb-6 md:ml-10 w-8/12 tracking-wider '> {par}</p></div>
     )
 }
