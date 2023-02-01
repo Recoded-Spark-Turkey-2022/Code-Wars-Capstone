1e42cf1b6764021d9563e274594f2e36a34f8ce6
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2023-02-01 12:52:58 +0300

    Added translation for Careers and About

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,22 +1,51 @@
-import React from 'react'
-import about from "./Rectangle.jpg"
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import about from './Rectangle.jpg';
 
 function About() {
+  const { t } = useTranslation();
+
   return (
-    <div className='pt-[4rem]'>
-    <p className="text-5xl pl-[4rem]">HEALING!</p>
-    <p className="text-2xl pl-[4rem] font-light leading-relaxed text-black text-opacity-50"  style={{fontFamily: 'Poppins, sans-serif'}}> some cool one liner !</p>
-    <p className=" pb-[10rem] pl-[4rem] pt-[4rem] lg:text-xl leading-relaxed text-gray-700 lg:w-[75rem]  md:w-[60rem] sm:w-[20rem] first-letter:"style={{fontFamily: 'Poppins, sans-serif'}} >At Healing, we believe there is a better way to do things. A more valuable way where customers are earned rather than bought. We&apos;re obsessively passionate about it, and our mission is to help people achieve it. We focus on search engine optimization. It&apos;s one of the least understood and least transparent aspects of great marketing, and we see that as an opportunity. We&apos;re excited to simplify SEO for everyone through our software, education, and community.</p>
-  
-    <div className="lg:flex lg:space-x-14 sm:space-x-10 justify-start pl-[4rem] bg-cyan-50 p-10 w-full " >
-    <img className="rounded-md sm:w-[27rem] lg:w-[21rem] md:w-[10rem] lg:h-[20rem] sm:h-[15rem]" alt='' src={about}/>
-    <div className="lg:inline-flex flex-col  space-y-6 items-start ">
-        <p className="w-96 h-1/6 lg:text-5xl md:text-4xl sm:text-3xl leading-10 text-gray-700 lg:w-[20rem]  md:w-[10rem]" >Our Founding</p>
-        <p className="lg:text-xl md:text-l  sm:text-sm leading-normal text-gray-700 lg:w-[40rem]  md:w-[20rem] sm:w-[20rem]  " style={{fontFamily: 'Poppins, sans-serif'}}>Healing was founded by Payam Abubakr in 2021. It was called Healing Online, and started as a blog and an online community where some of the world&apos;s therapists shared their research and ideas. We launched the Beginner&apos;s Guide to Therapy and our first study, and that hub of industry expertise transformed into a small consulting firm and led us to create the Online Therapist of today!</p>
+    <div className="pt-[4rem]">
+      <p className="text-5xl pl-[4rem]">{t('HEALING!')}</p>
+      <p
+        className="text-2xl pl-[4rem] font-light leading-relaxed text-black text-opacity-50"
+        style={{ fontFamily: 'Poppins, sans-serif' }}
+      >
+        {' '}
+        {t('some cool one liner !')}
+      </p>
+      <p
+        className=" pb-[10rem] pl-[4rem] pt-[4rem] lg:text-xl leading-relaxed text-gray-700 lg:w-[75rem]  md:w-[60rem] sm:w-[20rem] first-letter:"
+        style={{ fontFamily: 'Poppins, sans-serif' }}
+      >
+        {t(
+          'At Healing, we believe there is a better way to do things. A more valuable way where customers are earned rather than bought. We&apos;re obsessively passionate about it, and our mission is to help people achieve it. We focus on search engine optimization. It&apos;s one of the least understood and least transparent aspects of great marketing, and we see that as an opportunity. We&apos;re excited to simplify SEO for everyone through our software, education, and community.'
+        )}
+      </p>
+
+      <div className="lg:flex lg:space-x-14 sm:space-x-10 justify-start pl-[4rem] bg-cyan-50 p-10 w-full ">
+        <img
+          className="rounded-md sm:w-[27rem] lg:w-[21rem] md:w-[10rem] lg:h-[20rem] sm:h-[15rem]"
+          alt=""
+          src={about}
+        />
+        <div className="lg:inline-flex flex-col  space-y-6 items-start ">
+          <p className="w-96 h-1/6 lg:text-5xl md:text-4xl sm:text-3xl leading-10 text-gray-700 lg:w-[20rem]  md:w-[10rem]">
+            {t('Our Founding')}
+          </p>
+          <p
+            className="lg:text-xl md:text-l  sm:text-sm leading-normal text-gray-700 lg:w-[40rem]  md:w-[20rem] sm:w-[20rem]  "
+            style={{ fontFamily: 'Poppins, sans-serif' }}
+          >
+            {t(
+              'Healing was founded by Payam Abubakr in 2021. It was called Healing Online, and started as a blog and an online community where some of the world&apos;s therapists shared their research and ideas. We launched the Beginner&apos;s Guide to Therapy and our first study, and that hub of industry expertise transformed into a small consulting firm and led us to create the Online Therapist of today!'
+            )}
+          </p>
+        </div>
+      </div>
     </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/booking/HeaderBooking.js
+++ src/components/booking/HeaderBooking.js
@@ -1,9 +1,19 @@
-const HeaderBooking=({Header , par})=> {
-    return (
+// import { useTranslation } from 'react-i18next';
 
-        <div style={{fontFamily: 'Poppins, sans-serif'}}><h1 className="pb-[0.5rem] text-4xl tracking-wider md:text-2x sm:text-4xl mt-10 mb-2 ml-20 md:ml-10 uppercase " > {Header} </h1>
-        <p className='tex-xl md:text-xl text-stone-500 sm:text-base ml-20 mb-6 md:ml-10 w-8/12 tracking-wider '> {par}</p></div>
-    )
-}
+const HeaderBooking = ({ Header, par }) => {
+    // const { t } = useTranslation();
+  return (
+    <div style={{ fontFamily: 'Poppins, sans-serif' }}>
+      <h1 className="pb-[0.5rem] text-4xl tracking-wider md:text-2x sm:text-4xl mt-10 mb-2 ml-20 md:ml-10 uppercase ">
+        {' '}
+        {Header}{' '}
+      </h1>
+      <p className="tex-xl md:text-xl text-stone-500 sm:text-base ml-20 mb-6 md:ml-10 w-8/12 tracking-wider ">
+        {' '}
+        {par}
+      </p>
+    </div>
+  );
+};
 
-export default HeaderBooking ;
+export default HeaderBooking;
